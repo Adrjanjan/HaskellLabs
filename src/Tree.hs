a780b0ee224f3e65bdc0c397732d4581cabe067e
Adrian Janakiewicz <adrjanjan@gmail.com>
2019-03-24 16:55:16 +0100

    isEmpty, isBinary, search, height, isBalanced, toString finished.

--- src/Tree.hs
+++ src/Tree.hs
@@ -48,5 +48,40 @@ insert :: Ord a => Tree a -> a -> Tree a
 insert Leaf to_put = Branch to_put Leaf Leaf
 insert (Branch v l r) to_put | to_put < v   = Branch v (insert l to_put) r
                              | to_put >= v  = Branch v l (insert r to_put)
+-------------------------------------------
+-- | Check Tree emptiness
+isEmpty:: Tree a -> Bool
+isEmpty Branch {} = False
+isEmpty Leaf = True
+-------------------------------------------
+-- | Check if Tree is binary
+isBinary:: (Eq a, Ord a) => Tree a -> Bool
+isBinary Leaf = True
+isBinary (Branch v l r) = (l == Leaf || value l < v) && (r == Leaf || value r >= v) && isBinary l && isBinary r
+-------------------------------------------
+-- | Check if element is in a Tree
+search:: (Eq a, Ord a) => Tree a -> a -> Bool
+search Leaf _ = False
+search (Branch v l r) to_find | to_find == v = True
+                              | to_find <  v = search l to_find
+                              | to_find >  v = search r to_find
 
+-------------------------------------------
+-- | Find Tree height
+height:: Tree a -> Int
+height Leaf = 0
+height (Branch _ l r) = 1 + max (height l) (height r)
+
+-------------------------------------------
+-- | Check if Tree is balanced
+isBalanced:: Tree a -> Bool
+isBalanced (Branch _ l r)
+  | abs (height l - height r) <= 1 = True
+  | otherwise = False
+
+-------------------------------------------
+-- | Parse Tree to String
+toString:: Show a => Tree a -> String
+toString Leaf = ""
+toString (Branch v l r) = show v ++ " (" ++ toString l ++ ", " ++ toString r ++ ")"
 
